c3a27b68f058d701700cb59201e977514fde775f
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-02-09 16:17:14 +0300

    уже хорошо работает

--- public/game.js
+++ public/game.js
@@ -1,5 +1,206 @@
-const socket = io();
+// const socket = io();
 
+// const config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: { y: 0 },
+//             debug: false
+//         }
+//     },
+//     scene: {
+//         preload: preload,
+//         create: create,
+//         update: update
+//     }
+// };
+
+// let player;
+// let cursors;
+// let otherPlayers = {};
+// let scoreText;
+// let background;
+// let rubies;
+// let gameStarted = false
+
+// const game = new Phaser.Game(config);
+
+// function preload() {
+//     this.load.image('player', 'https://labs.phaser.io/assets/sprites/mushroom2.png');
+//     this.load.image('object', 'https://labs.phaser.io/assets/sprites/melon.png');
+
+//     this.load.image('sky', 'https://labs.phaser.io/assets/skies/sky1.png');
+// }
+
+// function create() {
+
+//     background = this.add.image(400, 300, 'sky');
+//     background.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
+
+//     cursors = this.input.keyboard.createCursorKeys();
+
+//     player = this.physics.add.sprite(400, 500, 'player').setCollideWorldBounds(true);
+
+
+//     scoreText = this.add.text(16, 16, 'Счет: 0', {
+//         fontSize: '32px',
+//         fill: '#fff'
+//     });
+
+//     countdownText = this.add.text(400, 300, '', {
+//         fontSize: '64px',
+//         fill: '#fff'
+//     }).setOrigin(0.5, 0.5);
+
+//     rubies = this.physics.add.group();
+
+//     socket.on('players', (players) => {
+//         const playerCount = Object.keys(players).length;
+
+
+//         if (playerCount >= 2 && gameStarted === false) {
+//             gameStarted = true
+//             startCountdown(this)
+//         }
+
+//         for (const id in players) {
+//             if (id === socket.id) continue;
+
+//             if (!otherPlayers[id]) {
+//                 otherPlayers[id] = this.physics.add.sprite(players[id].x, players[id].y, 'player');
+//             } else {
+//                 otherPlayers[id].setPosition(players[id].x, players[id].y);
+//             }
+//         }
+//     });
+
+//     socket.on('players', (players) => {
+//         for (const id in players) {
+//             if (id === socket.id) {
+//                 scoreText.setText('Счет: ' + players[id].score);
+//             }
+//         }
+//     });
+// }
+
+// function update() {
+//     // Движение игрока
+//     if (cursors.left.isDown) {
+//         player.setVelocityX(-160);
+//     } else if (cursors.right.isDown) {
+//         player.setVelocityX(160);
+//     } else {
+//         player.setVelocityX(0);
+//     }
+
+//     if (cursors.up.isDown) {
+//         player.setVelocityY(-160);
+//     } else if (cursors.down.isDown) {
+//         player.setVelocityY(160);
+//     } else {
+//         player.setVelocityY(0);
+//     }
+
+//     socket.emit('move', { x: player.x, y: player.y });
+// }
+
+// function changeBackground() {
+//     const randomIndex = Phaser.Math.Between(0, backgrounds.length - 1);
+//     background.setTexture(backgrounds[randomIndex]);
+// }
+
+// function startCountdown(sceneInstance) {
+//     countdown = 3
+//     const timer = sceneInstance.time.addEvent({
+//         delay: 1000,
+//         callback: () => {
+//             if (countdown > 0) {
+//                 countdownText.setText(countdown.toString());
+//                 countdown--;
+//             } else {
+//                 countdownText.setText('');
+//                 timer.remove(false);
+//                 spawnRubies(sceneInstance)
+//             }
+//         },
+//         loop: true
+//     });
+//     socket.emit('startCountdown')
+// }
+
+// function spawnRubies(sceneInstance) {
+//     for (let i = 0; i < 10; i++) {
+//         const x = Phaser.Math.Between(50, 750);
+//         const y = Phaser.Math.Between(50, 550);
+//         const ruby = rubies.create(x, y, 'object');
+//         ruby.setCollideWorldBounds(true);
+//         ruby.ownerId = socket.id;
+//         ruby.id = Phaser.Math.RND.uuid()
+//     }
+
+//     sceneInstance.physics.add.overlap(player, rubies, (_, ruby) => {
+//         if (ruby && ruby.active && ruby.ownerId === socket.id) {
+//             collectRuby(ruby)
+//         }
+//     }, null, sceneInstance)
+// }
+
+// function collectRuby(Ruby) {
+//     console.log(rubies.getChildren())
+//     console.log(Ruby)
+//     const ruby = rubies.getChildren().find(r => r.id === Ruby.id)
+//     if (ruby) {
+//         socket.emit('collectRuby', ruby.id);
+//     }
+// }
+
+// socket.on('removeRuby', (rubyId) => {
+//     const ruby = rubies.getChildren().find((r) => r.id === rubyId);
+//     if (ruby) {
+//         ruby.destroy();
+//         rubies.remove(ruby)
+//         if (rubies.getChildren().length === 0) {
+//             socket.emit('playerWon', socket.id);
+//         }
+//     }
+// });
+
+
+// // Слушаем сообщение о победителе
+// socket.on('gameOver', (winnerId) => {
+//     if (winnerId === socket.id) {
+//         alert('Поздравляем! Вы победили!');
+//     } else {
+//         alert(`Игрок ${winnerId} победил!`);
+//     }
+//     game.scene.pause();
+// });
+// socket.on('startCountdown', () => {
+//     startCountdown(this.scene)
+// });
+
+// // При сборе рубина
+// socket.on('collectRuby', (rubyId) => {
+//     console.log(rubyId)
+//     const ruby = rubies.getChildren().find(r => r.id === rubyId)
+//     console.log(ruby)
+//     ruby.destroy();
+//     rubies.remove(ruby)
+//     console.log(rubies.getChildren())
+//     console.log(rubies.getChildren().length)
+//     if (rubies.getChildren().length === 0) {
+//         console.log("я зашел")
+//         socket.emit('playerWon', socket.id);
+//     }
+
+//     socket.emit('score', player.id)
+//     player.score += 10;
+// });
+
+const socket = io();
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -8,14 +209,14 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: false
-        }
+            debug: false,
+        },
     },
     scene: {
         preload: preload,
         create: create,
-        update: update
-    }
+        update: update,
+    },
 };
 
 let player;
@@ -23,55 +224,43 @@ let cursors;
 let otherPlayers = {};
 let scoreText;
 let background;
-let rubies;
-const backgrounds = ["background1", "background2", "background3", "background4"];
-let gameStarted = false
+let rubiesGroup;
+let gameStarted = false;
 
 const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    this.load.image('object', 'https://labs.phaser.io/assets/sprites/shinyball.png');
-
-    this.load.image('background1', 'https://labs.phaser.io/assets/skies/space4.png');
-    this.load.image('background2', 'https://labs.phaser.io/assets/skies/space3.png');
-    this.load.image('background3', 'https://labs.phaser.io/assets/skies/space2.png');
-    this.load.image('background4', 'https://labs.phaser.io/assets/skies/space1.png');
+    this.load.image('player', 'https://labs.phaser.io/assets/sprites/mushroom2.png');
+    this.load.image('object', 'https://labs.phaser.io/assets/sprites/melon.png');
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/sky1.png');
 }
 
 function create() {
-
-    background = this.add.image(400, 400, backgrounds[0]);
+    background = this.add.image(400, 300, 'sky');
     background.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 
-    changeBackground()
-
-
     cursors = this.input.keyboard.createCursorKeys();
-
-
     player = this.physics.add.sprite(400, 500, 'player').setCollideWorldBounds(true);
-
 
     scoreText = this.add.text(16, 16, 'Счет: 0', {
         fontSize: '32px',
-        fill: '#fff'
+        fill: '#fff',
     });
 
     countdownText = this.add.text(400, 300, '', {
         fontSize: '64px',
-        fill: '#fff'
+        fill: '#fff',
     }).setOrigin(0.5, 0.5);
 
-    rubies = this.physics.add.group();
+    rubiesGroup = this.physics.add.group();
 
+    // Обработка списка игроков
     socket.on('players', (players) => {
         const playerCount = Object.keys(players).length;
 
-
-        if (playerCount >= 2 && gameStarted === false) {
-            gameStarted = true
-            startCountdown(this)
+        if (playerCount >= 2 && !gameStarted) {
+            gameStarted = true;
+            startCountdown(this);
         }
 
         for (const id in players) {
@@ -83,19 +272,35 @@ function create() {
                 otherPlayers[id].setPosition(players[id].x, players[id].y);
             }
         }
+
+        if (players[socket.id]) {
+            scoreText.setText('Счет: ' + players[socket.id].score);
+        }
     });
 
-    socket.on('players', (players) => {
-        for (const id in players) {
-            if (id === socket.id) {
-                scoreText.setText('Счет: ' + players[id].score);
-            }
+    // Обработка списка рубинов
+    socket.on('rubies', (rubiesData) => {
+        rubiesGroup.clear(true, true); // Очищаем текущие рубины
+
+        for (const rubyId in rubiesData) {
+            const ruby = rubiesData[rubyId];
+            rubiesGroup.create(ruby.x, ruby.y, 'object').setData('id', rubyId);
         }
+
+        this.physics.add.overlap(player, rubiesGroup, collectRuby, null, this);
+    });
+
+    socket.on('gameOver', (winnerId) => {
+        if (winnerId === socket.id) {
+            alert('Поздравляем! Вы победили!');
+        } else {
+            alert(`Игрок ${winnerId} победил!`);
+        }
+        game.scene.pause();
     });
 }
 
 function update() {
-    // Движение игрока
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
     } else if (cursors.right.isDown) {
@@ -115,13 +320,8 @@ function update() {
     socket.emit('move', { x: player.x, y: player.y });
 }
 
-function changeBackground() {
-    const randomIndex = Phaser.Math.Between(0, backgrounds.length - 1);
-    background.setTexture(backgrounds[randomIndex]);
-}
-
 function startCountdown(sceneInstance) {
-    countdown = 3
+    let countdown = 3;
     const timer = sceneInstance.time.addEvent({
         delay: 1000,
         callback: () => {
@@ -131,79 +331,16 @@ function startCountdown(sceneInstance) {
             } else {
                 countdownText.setText('');
                 timer.remove(false);
-                spawnRubies(sceneInstance)
             }
         },
-        loop: true
+        loop: true,
     });
-    socket.emit('startCountdown')
+
+    socket.emit('startCountdown');
 }
 
-function spawnRubies(sceneInstance) {
-    for (let i = 0; i < 10; i++) {
-        const x = Phaser.Math.Between(50, 750);
-        const y = Phaser.Math.Between(50, 550);
-        const ruby = rubies.create(x, y, 'object');
-        ruby.setCollideWorldBounds(true);
-        ruby.ownerId = socket.id;
-        ruby.id = Phaser.Math.RND.uuid()
-    }
-
-    sceneInstance.physics.add.overlap(player, rubies, (_, ruby) => {
-        if (ruby && ruby.active && ruby.ownerId === socket.id) {
-            collectRuby(ruby)
-        }
-    }, null, sceneInstance)
+function collectRuby(_, ruby) {
+    const rubyId = ruby.getData('id');
+    socket.emit('collectRuby', rubyId); // Уведомляем сервер о сборе рубина
+    ruby.destroy(); // Удаляем рубин с клиента
 }
-
-function collectRuby(Ruby) {
-    console.log(rubies.getChildren())
-    console.log(Ruby)
-    const ruby = rubies.getChildren().find(r => r.id === Ruby.id)
-    if (ruby) {
-        socket.emit('collectRuby', ruby.id);
-    }
-}
-
-socket.on('removeRuby', (rubyId) => {
-    const ruby = rubies.getChildren().find((r) => r.id === rubyId);
-    if (ruby) {
-        ruby.destroy();
-        rubies.remove(ruby)
-        if (rubies.getChildren().length === 0) {
-            socket.emit('playerWon', socket.id);
-        }
-    }
-});
-
-
-// Слушаем сообщение о победителе
-socket.on('gameOver', (winnerId) => {
-    if (winnerId === socket.id) {
-        alert('Поздравляем! Вы победили!');
-    } else {
-        alert(`Игрок ${winnerId} победил!`);
-    }
-    game.scene.pause();
-});
-socket.on('startCountdown', () => {
-    startCountdown(this.scene)
-});
-
-// При сборе рубина
-socket.on('collectRuby', (rubyId) => {
-    console.log(rubyId)
-    const ruby = rubies.getChildren().find(r => r.id === rubyId)
-    console.log(ruby)
-    ruby.destroy();
-    rubies.remove(ruby)
-    console.log(rubies.getChildren())
-    console.log(rubies.getChildren().length)
-    if (rubies.getChildren().length === 0) {
-        console.log("я зашел")
-        socket.emit('playerWon', socket.id);
-    }
-
-    socket.emit('score', player.id)
-    player.score += 10;
-});
--- server.js
+++ server.js
@@ -1,6 +1,65 @@
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
+// app.use(express.static('public'));
+
+// let players = {};
+
+// io.on('connection', (socket) => {
+//     console.log('Новый игрок подключился: ' + socket.id);
+
+//     players[socket.id] = {
+//         x: 400,
+//         y: 500,
+//         score: 0 
+//     };
+
+//     io.emit('players', players);
+
+//     socket.on('move', (data) => {
+//         if (players[socket.id]) {
+//             players[socket.id].x = data.x;
+//             players[socket.id].y = data.y;
+//             io.emit('players', players);
+//         }
+//     });
+
+//     socket.on('score', () => {
+//         if (players[socket.id]) {
+//             players[socket.id].score += 10;
+//             io.emit('players', players);
+//         }
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('Игрок отключился: ' + socket.id);
+//         delete players[socket.id];
+//         io.emit('players', players);
+//     });
+
+//     socket.on('collectRuby', (rubyId) => {
+//         io.emit('collectRuby', rubyId);
+//     })
+
+//     socket.on('playerWon', (winnerId) => {
+//         console.log("Игрок ${winnerId} собрал все рубины и победил!");
+//         io.emit('gameOver', winnerId);
+//     });
+// });
+
+// server.listen(8000, () => {
+//     console.log('Сервер запущен на http://localhost:8000');
+// });
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { v4: uuidv4 } = require('uuid'); // Импортируем функцию для генерации UUID
 
 const app = express();
 const server = http.createServer(app);
@@ -9,17 +68,27 @@ const io = socketIo(server);
 app.use(express.static('public'));
 
 let players = {};
+let rubies = {}; // Общие рубины для всех игроков
+let gameStarted = false; // Флаг для начала игры
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('Новый игрок подключился: ' + socket.id);
-
     players[socket.id] = {
         x: 400,
         y: 500,
-        score: 0 
+        score: 0,
     };
-
     io.emit('players', players);
+
+    console.log(Object.keys(players).length)
+    // Проверяем, достаточно ли игроков для начала игры
+    if (Object.keys(players).length == 2 && !gameStarted) {
+        gameStarted = true;
+        console.log('Начинаем игру! Генерация рубинов...');
+        spawnRubies();
+        await sleep(2000); // Пауза на 2 секунды
+        io.emit('rubies', rubies); // Отправляем список рубинов всем игрокам
+    }
 
     socket.on('move', (data) => {
         if (players[socket.id]) {
@@ -29,10 +98,12 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('score', () => {
-        if (players[socket.id]) {
+    socket.on('collectRuby', (rubyId) => {
+        if (rubies[rubyId]) {
+            delete rubies[rubyId];
             players[socket.id].score += 10;
-            io.emit('players', players);
+            io.emit('players', players); // Обновляем список игроков
+            io.emit('rubies', rubies); // Обновляем список рубинов
         }
     });
 
@@ -40,18 +111,37 @@ io.on('connection', (socket) => {
         console.log('Игрок отключился: ' + socket.id);
         delete players[socket.id];
         io.emit('players', players);
+
+        // Если игроков стало меньше двух, сбрасываем игру
+        if (Object.keys(players).length < 2) {
+            gameStarted = false;
+            rubies = {}; // Очищаем рубины
+            io.emit('rubies', rubies); // Уведомляем клиентов об очистке рубинов
+        }
     });
 
-    socket.on('collectRuby', (rubyId) => {
-        io.emit('collectRuby', rubyId);
-    })
-
     socket.on('playerWon', (winnerId) => {
-        console.log("Игрок ${winnerId} собрал все рубины и победил!");
+        console.log(`Игрок ${winnerId} собрал все рубины и победил!`);
         io.emit('gameOver', winnerId);
     });
 });
 
+function spawnRubies() {
+    for (let i = 0; i < 10; i++) {
+        const rubyId = uuidv4(); // Генерируем уникальный ID для рубина
+        rubies[rubyId] = {
+            id: rubyId,
+            x: Math.floor(Math.random() * (750 - 50 + 1)) + 50, // Случайная позиция по X
+            y: Math.floor(Math.random() * (550 - 50 + 1)) + 50, // Случайная позиция по Y
+        };
+    }
+    console.log('Сгенерированные рубины:', rubies);
+}
+
 server.listen(8000, () => {
     console.log('Сервер запущен на http://localhost:8000');
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
