faa0545b3e17f81765399f046738ee2f50d1fa20
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-02-09 16:31:32 +0300

    промежуточная точка работает таймер

--- server.js
+++ server.js
@@ -1,61 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// app.use(express.static('public'));
-
-// let players = {};
-
-// io.on('connection', (socket) => {
-//     console.log('Новый игрок подключился: ' + socket.id);
-
-//     players[socket.id] = {
-//         x: 400,
-//         y: 500,
-//         score: 0 
-//     };
-
-//     io.emit('players', players);
-
-//     socket.on('move', (data) => {
-//         if (players[socket.id]) {
-//             players[socket.id].x = data.x;
-//             players[socket.id].y = data.y;
-//             io.emit('players', players);
-//         }
-//     });
-
-//     socket.on('score', () => {
-//         if (players[socket.id]) {
-//             players[socket.id].score += 10;
-//             io.emit('players', players);
-//         }
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('Игрок отключился: ' + socket.id);
-//         delete players[socket.id];
-//         io.emit('players', players);
-//     });
-
-//     socket.on('collectRuby', (rubyId) => {
-//         io.emit('collectRuby', rubyId);
-//     })
-
-//     socket.on('playerWon', (winnerId) => {
-//         console.log("Игрок ${winnerId} собрал все рубины и победил!");
-//         io.emit('gameOver', winnerId);
-//     });
-// });
-
-// server.listen(8000, () => {
-//     console.log('Сервер запущен на http://localhost:8000');
-// });
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -80,14 +22,17 @@ io.on('connection', async (socket) => {
     };
     io.emit('players', players);
 
-    console.log(Object.keys(players).length)
     // Проверяем, достаточно ли игроков для начала игры
-    if (Object.keys(players).length == 2 && !gameStarted) {
+    if (Object.keys(players).length === 2 && !gameStarted) {
         gameStarted = true;
         console.log('Начинаем игру! Генерация рубинов...');
         spawnRubies();
-        await sleep(2000); // Пауза на 2 секунды
+        await sleep(2000); // Пауза на 2 секунды перед отправкой рубинов
         io.emit('rubies', rubies); // Отправляем список рубинов всем игрокам
+        io.emit('startCountdown'); // Начинаем обратный отсчет для всех клиентов
+        setTimeout(() => {
+            io.emit('startGame'); // Разрешаем движение после таймера
+        }, 3000); // Длительность таймера (3 секунды)
     }
 
     socket.on('move', (data) => {
@@ -111,8 +56,6 @@ io.on('connection', async (socket) => {
         console.log('Игрок отключился: ' + socket.id);
         delete players[socket.id];
         io.emit('players', players);
-
-        // Если игроков стало меньше двух, сбрасываем игру
         if (Object.keys(players).length < 2) {
             gameStarted = false;
             rubies = {}; // Очищаем рубины
